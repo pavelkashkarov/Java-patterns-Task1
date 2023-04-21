9f79fbfbb044e1656289770658cb124f1363168f
Pavel Kashkarov <PKashkarov@croc.ru>
2023-04-21 12:43:23 +0300

    initial commit

---
+++ src/Person.java
@@ -0,0 +1,74 @@
+import java.util.Objects;
+
+public class Person {
+    private final String name;
+    private final String surname;
+    private int age;
+    private String city;
+
+    public Person(String name, String surname) {
+        this(name, surname, -1);
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    public boolean hasAge() {
+        return age != -1;
+    }
+
+    public boolean hasCity() {
+        return city != null;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                .setAge(0)
+                .setCity(city);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, city);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
